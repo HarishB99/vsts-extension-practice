4c152a1acff2736254a63726076f34ed2c2a3a7b
Harish <harish_balamurugan@yahoo.com.sg>
2018-09-11 14:59:46 +0800

    Install vsts-task-lib

--- buildAndReleaseTask/index.js
+++ buildAndReleaseTask/index.js
@@ -6,11 +6,13 @@ process.argv.forEach((value, index) => {
 console.log();
 console.log('[+] Printing out process arguments: Complete');
 console.log();
+
 console.log('[i] Printing out process environments: Started');
 // console.log(process.env);
 console.log(`samplestring: ${process.env.INPUT_SAMPLESTRING}`);
 console.log('[+] Printing out process environments: Complete');
 console.log();
+
 console.log('[i] Performing arbitrary code execution: Started');
 let num = 1;
 num++;
@@ -18,10 +20,12 @@ console.log(`num = ${num}`);
 console.log(`typeof num = ${typeof num}`);
 console.log('[+] Performing arbitrary code execution: Complete');
 console.log();
+
 console.log('[i] Setting process variable, testvar: Started');
 console.log('##vso[task.setvariable variable=testvar;]testvalue');
 console.log('[+] Setting process variable, testvar: Complete');
 console.log();
+
 console.log('[i] Printing out final test message: Started');
 console.log('[i] My name is Optimus Prime. This is a test message for build and release tasks.');
 console.log('[+] Printing out final test message: Complete');
