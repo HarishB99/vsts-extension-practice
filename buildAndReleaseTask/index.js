71cdd4a150ff739d1a24891fee9708f17367c233
Harish <harish_balamurugan@yahoo.com.sg>
2018-09-17 09:54:03 +0800

    Fifth successful extension - Send https request to REST endpoint
    Problem - Need to follow redirects

--- buildAndReleaseTask/index.js
+++ buildAndReleaseTask/index.js
@@ -14,14 +14,35 @@ try {
     let approval_timeout = task.getInput('approval_timeout', true);
     console.log(`[i] approval_timeout ${approval_timeout}`);
     console.log('[+] Storing input variables: Complete');
+    // console.log();
+    // console.log('[i] Printing out process variables: Started');
+    // console.log();
+    // console.log(process.env);
+    // console.log();
+    // console.log('[+] Printing out process variables: Completed');
     console.log();
-    console.log(`[i] Sending request to https://www.google.com/: Started`);
-    https.get('https://www.google.com/', (response) => {
-        console.log(`[i] ${response.statusCode}`);
-        console.log('[+] Sending request to https://www.google.com/: Complete');
+    const url = process.env.ENDPOINT_URL_SYSTEMVSSCONNECTION.concat(process.env.BUILD_PROJECTNAME).concat('/_apis/Release/definitions');
+    console.log(`[i] Sending request to ${url}: Started`);
+    // https.get(`${process.env.ENDPOINT_URL_SYSTEMVSSCONNECTION}${process.env.BUILD_PROJECTNAME}/_apis/Release/defintions`, (response) => {
+    //     console.log(`[i] ${response.statusCode}`);
+    //     console.log('[+] Sending request: Complete');
+    //     console.log();
+    //     console.log('[+] Executing task: Completed');
+    // });
+    https.get(url, (response) => {
+        console.log('[i] statusCode: ', response.statusCode);
+        console.log('[i] headers: ', response.headers);
+        
+        response.on('data', (d) => {
+            process.stdout.write(d);
+        });
+        console.log(`[+] Sending request to ${url}: Complete`);
+        console.log();
+        console.log('[+] Executing task: Completed');
+    }).on('error', (e) => {
+        console.log(`[-] Sending request to ${url}: Failure`);
+        console.error(`Reason: ${e}`);
     });
-    console.log();
-    console.log('[+] Executing task: Completed');
     console.log();
 } catch (error) {
     console.log();
