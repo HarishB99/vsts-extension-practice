1f5b0a8556ddcef1fc943733ed445878bca91cd6
Harish <harish_balamurugan@yahoo.com.sg>
2018-09-17 09:19:16 +0800

    Harish Release Extension Version 0.0.0.25 - Fourth version of working extension (tested)

--- buildAndReleaseTask/index.js
+++ buildAndReleaseTask/index.js
@@ -1,36 +1,30 @@
 const task = require('vsts-task-lib/task');
-const colors = require('colors');
 const http = require('http');
-
-colors.setTheme({
-    info: 'cyan',
-    success: 'green',
-    warn: 'yellow',
-    error: 'red'
-});
+const https = require('https');
 
 try {
     console.log();
-    console.log(colors.info('[i] Executing task: Started'));
+    console.log('[i] Executing task: Started');
     console.log();
-    console.log(colors.info('[i] Storing input variables: Started'));
+    console.log('[i] Storing input variables: Started');
     let primary_approver = task.getInput('primary_approver', true);
-    console.log(colors.info(`[i] primary_approver ${primary_approver}`));
+    console.log(`[i] primary_approver ${primary_approver}`);
     let secondary_approver = task.getInput('secondary_approver', true);
-    console.log(colors.info(`[i] secondary_approver ${secondary_approver}`));
+    console.log(`[i] secondary_approver ${secondary_approver}`);
     let approval_timeout = task.getInput('approval_timeout', true);
-    console.log(colors.info(`[i] approval_timeout ${approval_timeout}`));
-    console.log(colors.success('[+] Storing input variables: Complete'));
+    console.log(`[i] approval_timeout ${approval_timeout}`);
+    console.log('[+] Storing input variables: Complete');
     console.log();
-    console.log(colors.info(`[i] Sending request to https://www.google.com/: Started`));
-    http.get('https://www.google.com/', (response) => {
-        console.log(colors.info(`[i] ${response.statusCode}`));
-        console.log(colors.success('[+] Sending request to https://www.google.com/: Complete'));
+    console.log(`[i] Sending request to https://www.google.com/: Started`);
+    https.get('https://www.google.com/', (response) => {
+        console.log(`[i] ${response.statusCode}`);
+        console.log('[+] Sending request to https://www.google.com/: Complete');
     });
     console.log();
-    console.log(colors.info('[+] Executing task: Completed'));
+    console.log('[+] Executing task: Completed');
     console.log();
 } catch (error) {
-    console.log(colors.error('[-] Executing task: Failure'));
+    console.log();
+    console.log('[-] Executing task: Failure');
     console.log(`Reason: ${error}`);
 }
