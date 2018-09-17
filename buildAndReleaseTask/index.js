ca521203cddf3fc6f4c53967d4815325121e0dc3
Harish <harish_balamurugan@yahoo.com.sg>
2018-09-17 08:59:41 +0800

    Harish Release Extension Version 0.0.0.23 - Second version of working extension (not tested yet)

--- buildAndReleaseTask/index.js
+++ buildAndReleaseTask/index.js
@@ -1,35 +1,36 @@
-console.log('[i] Communicating with VSTS REST endpoint: Started');
-console.log(process.env);
-console.log('[+] Communicating with VSTS REST endpoint: Complete');
+const task = require('vsts-task-lib/task');
+const colors = require('colors');
+const http = require('http');
 
-console.log('[i] Printing out process arguments: Started');
-console.log();
-process.argv.forEach((value, index) => {
-    console.log(`${index}: ${value}`);
+colors.setTheme({
+    info: 'cyan',
+    success: 'green',
+    warn: 'yellow',
+    error: 'red'
 });
-console.log();
-console.log('[+] Printing out process arguments: Complete');
-console.log();
 
-console.log('[i] Printing out process environments: Started');
-// console.log(process.env);
-console.log(`samplestring: ${process.env.INPUT_SAMPLESTRING}`);
-console.log('[+] Printing out process environments: Complete');
-console.log();
-
-console.log('[i] Performing arbitrary code execution: Started');
-let num = 1;
-num++;
-console.log(`num = ${num}`);
-console.log(`typeof num = ${typeof num}`);
-console.log('[+] Performing arbitrary code execution: Complete');
-console.log();
-
-console.log('[i] Setting process variable, testvar: Started');
-console.log('##vso[task.setvariable variable=testvar;]testvalue');
-console.log('[+] Setting process variable, testvar: Complete');
-console.log();
-
-console.log('[i] Printing out final test message: Started');
-console.log('[i] My name is Optimus Prime. This is a test message for build and release tasks.');
-console.log('[+] Printing out final test message: Complete');
+try {
+    console.log();
+    console.log(colors.info('[i] Executing task: Started'));
+    console.log();
+    console.log(colors.info('[i] Storing input variables: Started'));
+    let primary_approver = task.getInput('primary_approver', true);
+    console.log(colors.info(`[i] primary_approver ${primary_approver}`));
+    let secondary_approver = task.getInput('secondary_approver', true);
+    console.log(colors.info(`[i] secondary_approver ${secondary_approver}`));
+    let approval_timeout = task.getInput('approval_timeout', true);
+    console.log(colors.info(`[i] approval_timeout ${approval_timeout}`));
+    console.log(colors.success('[+] Storing input variables: Complete'));
+    console.log();
+    console.log(colors.info(`[i] Sending request to https://www.google.com/: Started`));
+    http.get('https://www.google.com/', (response) => {
+        console.log(colors.info(`[i] ${response.statusCode}`));
+        console.log(colors.success('[+] Sending request to https://www.google.com/: Complete'));
+    });
+    console.log();
+    console.log(colors.info('[+] Executing task: Completed'));
+    console.log();
+} catch (error) {
+    console.log(colors.error('[-] Executing task: Failure'));
+    console.log(`Reason: ${error}`);
+}
