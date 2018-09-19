f5822887cd9ef67f10c02340c5461fa3109ad78d
Harish <harish_balamurugan@yahoo.com.sg>
2018-09-19 13:25:05 +0800

    Use SDKs to successfully retrieve releases

--- buildAndReleaseTask/index.js
+++ buildAndReleaseTask/index.js
@@ -5,19 +5,36 @@ const https = require('follow-redirects').https;
 const request = require('request');
 const vsts = require('azure-devops-node-api/CoreApi');
 const release = require('azure-devops-node-api/ReleaseApi').ReleaseApi;
+const azure_devops_api = require('azure-devops-node-api');
 
 try {
+    let authHandler = azure_devops_api.getBearerHandler(
+        task.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false));
+    // console.log(`System_Teamfoundationuri: ${process.env.SYSTEM_TEAMFOUNDATIONSERVERURI}`);
+    let connection = new azure_devops_api.WebApi(process.env.SYSTEM_TEAMFOUNDATIONSERVERURI, authHandler);
+    let release_api = connection.getReleaseApi();
+    release_api.then(api => {
+        return api.getReleases();
+    }).then(releases => {
+        console.log(`releases: ${releases}`);
+        releases.forEach(release => {
+            console.log(`release definition: ${release.createdOn.toDateString()}`);
+            console.log(`release definition: ${release.releaseDefinition.id}`);
+        });
+    }).catch(error => {
+        console.error(`error: ${error}`);
+    });
     // new vsts.CoreApi().rest
     console.log();
     console.log('[i] Executing task: Started');
     console.log();
     console.log('[i] Storing input variables: Started');
     let primary_approver = task.getInput('primary_approver', true);
-    console.log(`[i] primary_approver ${primary_approver}`);
+    console.log(`[i] primary_approver: ${primary_approver}`);
     let secondary_approver = task.getInput('secondary_approver', true);
-    console.log(`[i] secondary_approver ${secondary_approver}`);
+    console.log(`[i] secondary_approver: ${secondary_approver}`);
     let approval_timeout = task.getInput('approval_timeout', true);
-    console.log(`[i] approval_timeout ${approval_timeout}`);
+    console.log(`[i] approval_timeout: ${approval_timeout}`);
     console.log('[+] Storing input variables: Complete');
     console.log();
     console.log('[i] Printing out process variables: Started');
@@ -26,31 +43,11 @@ try {
     console.log();
     console.log('[+] Printing out process variables: Completed');
     console.log();
-    // const url = process.env.ENDPOINT_URL_SYSTEMVSSCONNECTION.concat(process.env.BUILD_PROJECTNAME).concat('/_apis/Release/definitions');
-    // console.log(`[i] Sending request to ${url}: Started`);
-    // https.get(`${process.env.ENDPOINT_URL_SYSTEMVSSCONNECTION}${process.env.BUILD_PROJECTNAME}/_apis/Release/defintions`, (response) => {
-    //     console.log(`[i] ${response.statusCode}`);
-    //     console.log('[+] Sending request: Complete');
-    //     console.log();
-    //     console.log('[+] Executing task: Completed');
-    // });
-    // request.post(url, {
-    //     followAllRedirects: true
-    // })
-    // .on('response', response => {
-    //     console.log('[i] statusCode: ', response.statusCode);
-    //     console.log('[i] headers: ', response.headers);
-        
-    //     response.on('data', (d) => {
-    //         process.stdout.write(d);
-    //     });
-    //     console.log(`[+] Sending request to ${url}: Complete`);
-    //     console.log();
-    //     console.log('[+] Executing task: Completed');
-    // }).on('error', (e) => {
-    //     console.log(`[-] Sending request to ${url}: Failure`);
-    //     console.error(`Reason: ${e}`);
-    // });
+    console.log('[i] Attempting to retrieve secret variable: Started');
+    let token = task.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false);
+    console.log(`token: ${token}`);
+    console.log('[+] Attempting to retrieve secret variable: Complete');
+    console.log();
     console.log('[i] Attempting to get release definitions: Started');
     new release(process.env.SYSTEM_TEAMFOUNDATIONSERVERURI).getReleaseDefinitions(process.env.BUILD_PROJECTNAME)
     .then(definitions => {
@@ -80,20 +77,6 @@ try {
         console.error('[-] Attempting to get releases: Failure');
         console.error(`Reason: ${error}`);
     });
-    // https.get(url, (response) => {
-    //     console.log('[i] statusCode: ', response.statusCode);
-    //     console.log('[i] headers: ', response.headers);
-        
-    //     response.on('data', (d) => {
-    //         process.stdout.write(d);
-    //     });
-    //     console.log(`[+] Sending request to ${url}: Complete`);
-    //     console.log();
-    //     console.log('[+] Executing task: Completed');
-    // }).on('error', (e) => {
-    //     console.log(`[-] Sending request to ${url}: Failure`);
-    //     console.error(`Reason: ${e}`);
-    // });
     console.log();
 } catch (error) {
     console.log();
