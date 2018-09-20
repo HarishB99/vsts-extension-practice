7cca3ed7eddc9a0fe90f9c2beedd3df30e28e76c
Harish <harish_balamurugan@yahoo.com.sg>
2018-09-20 10:05:08 +0800

    Organise output for debugging purposes

--- buildAndReleaseTask/index.js
+++ buildAndReleaseTask/index.js
@@ -12,35 +12,39 @@ try {
     console.log('[i] Executing task: Started');
     console.log();
     console.log('[i] Storing input variables: Started');
+    console.log();
     let primary_approver = task.getInput('primary_approver', true);
     console.log(`[i] primary_approver: ${primary_approver}`);
     let secondary_approver = task.getInput('secondary_approver', true);
     console.log(`[i] secondary_approver: ${secondary_approver}`);
     let approval_timeout = task.getInput('approval_timeout', true);
     console.log(`[i] approval_timeout: ${approval_timeout}`);
+    console.log();
     console.log('[+] Storing input variables: Complete');
+    console.log();
     let authHandler = azure_devops_api.getBearerHandler(
         task.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false));
     let connection = new azure_devops_api.WebApi(process.env.SYSTEM_TEAMFOUNDATIONSERVERURI, authHandler);
+    console.log('[i] Get details from REST endpoint: Started');
+    console.log();
     connection.getReleaseApi()
     .then(api => {
         return api.getReleaseDefinition(process.env.SYSTEM_TEAMPROJECT, process.env.RELEASE_DEFINITIONID);
     }).then(definition => {
-        // const releases = arr[0];
-        // const releaseDefinition = arr[1];
-        // console.log(`releases: ${JSON.stringify(releases)}`);
-        // console.log(`releases definitions: ${JSON.stringify(releaseDefinition)}`);
-        // releases.forEach(release => {
-            // console.log(`release definition: ${release.createdOn.toDateString()}`);
-            // console.log(`release definition: ${release.releaseDefinition.id}`);
-            // console.log(`release id: ${release.id}`);
-        // });
-        console.log(`Post deploy approvals: ${JSON.stringify(definition.environments[0].postDeployApprovals)}`);
-        console.log(`Pre deploy approvals: ${JSON.stringify(definition.environments[0].preDeployApprovals)}`);
+        console.log(`[i] Release definition: ${JSON.stringify(definition)}`);
+        console.log(`[i] Post deploy approvals: ${JSON.stringify(definition.environments[0].postDeployApprovals)}`);
+        console.log(`[i] Pre deploy approvals: ${JSON.stringify(definition.environments[0].preDeployApprovals)}`);
+        console.log();
+        console.log('[+] Get details from REST endpoint: Complete');
+        console.log();
+        console.log('[+] Executing task: Complete');
     }).catch(error => {
-        console.error(`error: ${error}`);
+        console.log('[-] Get details from REST endpoint: Failure');
+        console.error(`Reason: ${error}`);
+        console.log();
+        console.log('[-] Executing task: Failure');
+        console.log('Reason: Get details from REST endpoint: Failure');
     });
-    console.log();
 } catch (error) {
     console.log();
     console.log('[-] Executing task: Failure');
