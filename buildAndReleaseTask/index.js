1161229c204ebb4e8d34e0583b04d35cdf996ce2
Harish <harish_balamurugan@yahoo.com.sg>
2018-09-24 16:59:33 +0800

    Create two approvers approving in sequence

--- buildAndReleaseTask/index.js
+++ buildAndReleaseTask/index.js
@@ -1,7 +1,4 @@
 const task = require('vsts-task-lib/task');
-const http = require('http');
-const https = require('follow-redirects').https;
-const request = require('request');
 const azure_devops_api = require('azure-devops-node-api');
 
 try {
@@ -12,11 +9,11 @@ try {
     console.log();
     let primary_approver = JSON.parse(task.getInput('primary_approver', true));
     console.log(`[i] primary_approver: ${primary_approver}`);
-    // console.log(`[i] typeof primary_approver: ${typeof primary_approver}`);
     let secondary_approver = JSON.parse(task.getInput('secondary_approver', true));
     console.log(`[i] secondary_approver: ${secondary_approver}`);
     let approval_timeout = task.getInput('approval_timeout', true);
     console.log(`[i] approval_timeout: ${approval_timeout}`);
+    console.log(`[i] typeof approval_timeout: ${typeof approval_timeout}`);
     console.log();
     console.log('[+] Storing input variables: Complete');
     console.log();
@@ -31,21 +28,13 @@ try {
             api.getRelease(process.env.SYSTEM_TEAMPROJECT, parseInt(process.env.RELEASE_RELEASEID)),
             connection.getReleaseApi()
         ]);
-        // return api.updateReleaseApprovals([{
-        //     approvalType: 'postDeploy',
-        //     approver: {
-        //         id: primary_approver
-        //     },
-        //     isAutomated: false,
-        //     status: 'reassigned'
-        // }], process.env.SYSTEM_TEAMPROJECT);
     })
     .then(results => {
         const release = results[0];
         const secondApi = results[1];
-        // console.log(`${JSON.stringify(release)}`);
         release.environments[1].postApprovalsSnapshot.approvalOptions = {
-            timeoutInMinutes: 30
+            timeoutInMinutes: 30,
+            releaseCreatorCanBeApprover: true
         };
 
         release.environments[1].postApprovalsSnapshot.approvals = [{
@@ -53,27 +42,19 @@ try {
             approver: {
                 id: primary_approver[0]
             },
-            isNotificationOn: true,
+            isNotificationOn: false,
             rank: 1
+        }, {
+            isAutomated: false,
+            approver: {
+                id: secondary_approver[0]
+            },
+            isNotificationOn: false,
+            rank: 2
         }];
         console.log(`Update release: ${JSON.stringify(release)}`);
         return secondApi.updateRelease(release, process.env.SYSTEM_TEAMPROJECT, release.id)
     })
-    // .then(approvals => {
-    //     console.log(`Updated Approvals: ${JSON.stringify(approvals)}`);
-    //     return connection.getReleaseApi();
-    // }).then(api => {
-    //     return api.getApprovals(process.env.SYSTEM_TEAMPROJECT);
-    // }).then(approvals => {
-    //     console.log(`Approvals: ${JSON.stringify(approvals)}`);
-    //     return connection.getReleaseApi();
-    // }).then(api => {
-    //     return api.getReleaseDefinition(process.env.SYSTEM_TEAMPROJECT, process.env.RELEASE_DEFINITIONID);
-    // }).then(definition => {
-    //     console.log(`Definitions: ${JSON.stringify(definition)}`);
-    //     console.log();
-    //     console.log(`[+] Update approvals: Complete`);
-    // })
     .then(release => {
         console.log(`After update: ${JSON.stringify(release)}`);
     })
