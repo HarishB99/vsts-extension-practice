58d8ab79845423616fa122d1121e58a6f8bce1fc
Harish <harish_balamurugan@yahoo.com.sg>
2018-09-26 10:17:23 +0800

    Organise console output and initialise eslint for js linting

--- buildAndReleaseTask/index.js
+++ buildAndReleaseTask/index.js
@@ -7,29 +7,27 @@ try {
     console.log();
     console.log('[i] Storing input variables: Started');
     console.log();
-    let primary_approver = JSON.parse(task.getInput('primary_approver', true));
+    const primary_approver = JSON.parse(task.getInput('primary_approver', true));
     console.log(`[i] primary_approver: ${primary_approver}`);
-    let secondary_approver = JSON.parse(task.getInput('secondary_approver', true));
+    const secondary_approver = JSON.parse(task.getInput('secondary_approver', true));
     console.log(`[i] secondary_approver: ${secondary_approver}`);
-    let approval_timeout = task.getInput('approval_timeout', true);
-    console.log(`[i] approval_timeout: ${approval_timeout}`);
+    const approval_timeout = parseInt(task.getInput('approval_timeout', true));
     console.log(`[i] typeof approval_timeout: ${typeof approval_timeout}`);
     console.log();
     console.log('[+] Storing input variables: Complete');
     console.log();
-    let authHandler = azure_devops_api.getBearerHandler(
+    const authHandler = azure_devops_api.getBearerHandler(
         task.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false));
-    let connection = new azure_devops_api.WebApi(process.env.SYSTEM_TEAMFOUNDATIONSERVERURI, authHandler);
-    console.log('[i] Update approvals: Started');
-    console.log();
-    connection.getReleaseApi()
-    .then(api => {
+    const connection = new azure_devops_api.WebApi(process.env.SYSTEM_TEAMFOUNDATIONSERVERURI, authHandler);
+    connection.getReleaseApi().then(api => {
+        console.log(`[i] Retrieve release info: Started`);
         return Promise.all([
             api.getRelease(process.env.SYSTEM_TEAMPROJECT, parseInt(process.env.RELEASE_RELEASEID)),
             connection.getReleaseApi()
         ]);
-    })
-    .then(results => {
+    }).then(results => {
+        console.log(`[+] Retrieve release info: Complete`);
+        console.log();
         const release = results[0];
         const secondApi = results[1];
         release.environments[1].postApprovalsSnapshot.approvalOptions = {
@@ -52,18 +50,20 @@ try {
             isNotificationOn: false,
             rank: 2
         }];
-        console.log(`Update release: ${JSON.stringify(release)}`);
+
+        console.log('[i] Update approvals: Started');
         return secondApi.updateRelease(release, process.env.SYSTEM_TEAMPROJECT, release.id)
-    })
-    .then(release => {
-        console.log(`After update: ${JSON.stringify(release)}`);
-    })
-    .catch(error => {
-        console.log('[-] Update approvals: Failure');
-        console.error(`Reason: ${error}`);
+    }).then(release => {
         console.log();
-        console.log('[-] Executing task: Failure');
-        console.log('Reason: Update approvals: Failure');
+        console.log(`Updated release: ${JSON.stringify(release)}`);
+        console.log();
+        console.log(`[+] Update approvals: Complete`);
+        console.log();
+        console.log(`[+] Executing task: Complete`);
+    }).catch(error => {
+        console.log();
+        console.log('[-] Promise rejected');
+        console.log(`Reason: ${error}`);
     });
 } catch (error) {
     console.log();
