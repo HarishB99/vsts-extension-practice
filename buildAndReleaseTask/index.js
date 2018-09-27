d2874aa4fcb95613c5a5c6a7ec70a44d0bb54fe4
Harish <harish_balamurugan@yahoo.com.sg>
2018-09-27 11:07:30 +0800

    Configure Primary approvers (regardless of the number fo primary approvers) for the next stage

--- buildAndReleaseTask/index.js
+++ buildAndReleaseTask/index.js
@@ -7,12 +7,35 @@ try {
     console.log();
     console.log('[i] Storing input variables: Started');
     console.log();
-    const primary_approver = JSON.parse(task.getInput('primary_approver', true));
-    console.log(`[i] primary_approver: ${primary_approver}`);
-    const secondary_approver = JSON.parse(task.getInput('secondary_approver', true));
-    console.log(`[i] secondary_approver: ${secondary_approver}`);
-    const approval_timeout = parseInt(task.getInput('approval_timeout', true));
-    console.log(`[i] typeof approval_timeout: ${typeof approval_timeout}`);
+        let pre_primary_approvers = null;
+        let pre_secondary_approvers = null;
+        let pre_approval_timeout = null;
+
+        let post_primary_approvers = null;
+        let post_secondary_approvers = null;
+        let post_approval_timeout = null;
+        const pre_enabled = task.getBoolInput('pre_enabled', true);
+            console.log(`[i] pre_enabled: ${pre_enabled}`);
+        const post_enabled = task.getBoolInput('post_enabled', true);
+            console.log(`[i] post_enabled: ${post_enabled}`);
+
+        if (pre_enabled) {
+            pre_primary_approvers = JSON.parse(task.getInput('pre_primary_approvers', true));
+                console.log(`[i] pre_primary_approvers: ${pre_primary_approvers}`);
+            pre_secondary_approvers = JSON.parse(task.getInput('pre_secondary_approvers', true));
+                console.log(`[i] pre_secondary_approvers: ${pre_secondary_approvers}`);
+            pre_approval_timeout = parseInt(task.getInput('pre_approval_timeout', true));
+                console.log(`[i] typeof pre_approval_timeout: ${typeof pre_approval_timeout}`);
+        }
+        
+        if (post_enabled) {
+            post_primary_approvers = JSON.parse(task.getInput('post_primary_approvers', true));
+                console.log(`[i] post_primary_approvers: ${post_primary_approvers}`); 
+            post_secondary_approvers = JSON.parse(task.getInput('post_secondary_approvers', true));
+                console.log(`[i] post_secondary_approvers: ${post_secondary_approvers}`);
+            post_approval_timeout = parseInt(task.getInput('post_approval_timeout', true));
+                console.log(`[i] typeof post_approval_timeout: ${typeof post_approval_timeout}`);
+        }
     console.log();
     console.log('[+] Storing input variables: Complete');
     console.log();
@@ -30,27 +53,61 @@ try {
         console.log();
         const release = results[0];
         const secondApi = results[1];
-        release.environments[1].postApprovalsSnapshot.approvalOptions = {
-            timeoutInMinutes: 30,
-            releaseCreatorCanBeApprover: true
-        };
+        let indexOfInterest = 0;
+        for (let i = 0; i < release.environments.length; i++) {
+            const environment = release.environments[i];
+            if (environment.name === process.env.RELEASE_ENVIRONMENTNAME) {
+                indexOfInterest = ++i;
+                break;
+            }
+        }
 
-        release.environments[1].postApprovalsSnapshot.approvals = [{
-            isAutomated: false,
-            approver: {
-                id: primary_approver[0]
-            },
-            isNotificationOn: false,
-            rank: 1
-        }, {
-            isAutomated: false,
-            approver: {
-                id: secondary_approver[0]
-            },
-            isNotificationOn: false,
-            rank: 2
-        }];
+        if (pre_enabled) {
+            release.environments[indexOfInterest].preApprovalsSnapshot.approvalOptions = {
+                timeoutInMinutes: pre_approval_timeout,
+                releaseCreatorCanBeApprover: true
+            };
 
+            const preApprovalsArray = [];
+
+            for (let i = 0; i < pre_primary_approvers.length; i++) {
+                const pre_primary_approver = pre_primary_approvers[i];
+                let currentIndex = i;
+                preApprovalsArray.push({
+                    isAutomated: false,
+                    approver: {
+                        id: pre_primary_approver
+                    },
+                    isNotificationOn: false,
+                    rank: ++currentIndex
+                });
+            }
+            release.environments[indexOfInterest].preApprovalsSnapshot.approvals = preApprovalsArray;
+        }
+        
+        if (post_enabled) {
+            release.environments[indexOfInterest].postApprovalsSnapshot.approvalOptions = {
+                timeoutInMinutes: post_approval_timeout,
+                releaseCreatorCanBeApprover: true
+            };
+
+            const postApprovalsArray = [];
+
+            for (let i = 0; i < post_primary_approvers.length; i++) {
+                const post_primary_approver = post_primary_approvers[i];
+                let currentIndex = i;
+                postApprovalsArray.push({
+                    isAutomated: false,
+                    approver: {
+                        id: post_primary_approver
+                    },
+                    isNotificationOn: false,
+                    rank: ++currentIndex
+                });
+            }
+            release.environments[indexOfInterest].postApprovalsSnapshot.approvals = postApprovalsArray;
+        }
+        console.log(`[i] Information to be updated: ${JSON.stringify(release)}`);
         console.log('[i] Update approvals: Started');
         return secondApi.updateRelease(release, process.env.SYSTEM_TEAMPROJECT, release.id)
     }).then(release => {
